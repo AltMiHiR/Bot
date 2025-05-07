1568ec1647e927f5d9fe4194dff4c3399ee849de
⌁ ⏤͟͞ 𝙄𝙩𝙯𝙈𝙞𝙝𝙞𝙧™ / <mihirxd13@gmail.com>
2025-05-07 20:30:39 +0530

    Update m.py

--- m.py
+++ m.py
@@ -14,8 +14,9 @@ API_ID = 25981592
 API_HASH = "709f3c9d34d83873d3c7e76cdd75b866"
 BOT_TOKEN = "6052402912:AAGGodIAMUUwU1f-LkTEWhT-taqlj31TYsw"
 SESSIONS = [
-    "BAGMcpgAqyJFu3AABcNw_D5uL54JMNCZKqNxykOLDGPPPHHesgRoHMM4XACKDxsYhF3r9yb7OgT7QiNL5WFQNuZb5beJuMD9w-Fkh0kz2tWQT0YtbShsdb-zW4HjrdGk3LUxb6JUG4_pZQZkq0a5VVSMfRlQtCvIgdjv4ngGjJgQokU1PrAcc99WuR4KmmzdaM4Iv9RrOmd43-yvcQp9M1N54CZeiMsQKD34aZWcf0kHYkns8jFZrUg53I81M2EOOvejE75Ms9awiE_mL8frUcj8QvshKdBvG7FjL5q_n_ACoby7jwV3aXS6fPbvjfshdEuPVHYPVQGjOVPBKxdNrYLyDcrePQAAAAG_Qa6bAA",
-    "BAGMcpgAjXnPqQXO_1welgUqE4gbpxpCEbgexm2KxTB_5Stm_VSLuzU_-fH5tOKufqgGaxcByPGU0_hM63NtY1WZy4KigXicUdmQ5yrGMVe6ZH4kYj1EnuxaKLHcPGhIyIENUJevPoMvFQjIVeKhg56nTUExrlWhu3hXieVP1uPkXGnR194TjdHytO71Mq7zf-AinLuYZaLMKBH93cXDgid0m_fPLY1M4Uczq9d86w45VqtWk9lTUI2-y5vJX_8sZ7BGn-mfqd1O9ix-wSh5DQ_87UXidh9n549vxROyfRmetBMuXb8016B13EGQrqIEBZXUHrWS7VfftchKqio7Cp2r4bsBsAAAAAHVxWm6AA"
+    "BQGMcpgAmRp608NdyIJ49a5cMpvtgSayQns6Yftews_Ow-snguTqn10rcFrVq8LX1ybBhWMlmvpUKyJVFyFlJ22ymQIMJ7NQJth-ex3qqZVRu39w83OAZZXo7FyhQq7ivAlhiEG14G81vdTp2tKLFHmSLNidwc5anLL-xVJaycpVH_j82Kv9SLsn3EhYwk1hzvnu-Zsatj5VNH7WH1RS2znPEZejZom4l9wqiegReKFQnQNh3ldZ9_saKKc0BJPqmH2ZWlWUMpuGH8pB8osCWz1ZBH-J1qZA7R-tL1s4YBPhQNjnEgKO6fo2OQ8iuN1ibv4WgC4RxYx4Vky3afKtlrDhoeno5gAAAAGh48SPAA",
+    "BQGMcpgApMUCqvW-_CVrwOjNC2wBaX6UAeRvDmQfeaWP4RUFn4mfNXlQLuRTQWTiV5H463aLMaq3vwERPiy0mQ1MyvSEXZNxrQJ_9_x0v0Bn1h1JvAYwt3hgbUmPnkmVZLqcsfC48jCsZb1gvTO_wSVsDXK9ah-CEC5F0FWClgZNewPai0I-FHEzY-opuH2n1PHMjeAJM7wANj10LmTAR5T0XrXLjJOd6fiBlJvykH3krB5KLAdTG8JqC-h_-c-B70I2VONMgVPewqy-kR9c1L5qEFRG7HzNztU6gqFYnDgomUGkkPPBwX9z_9lHKAF2G9UQAGumhnfTU35VU_EdgvMFavfi1AAAAAHRRT_uAA",
+    "BAGMcpgAAzAYrfBgGVT0sP2hTaqE8a1zZkhFhTR2yq1xrh94SGlrzvPeDqgOy4HUpcvRUdz3MBTUB2oTCM_F645kwnyyoLFocQ0ViBiQHTNTZ9uCcirC6k4AEldWfrXLEFGrNIt3_TXSytegKSvBMqjXH9nrwhSPqrTpBK9X9Fkg3H35j22RnaFzBpmE3ZSpGDU4ptTYdprF_VFHNb7bdbrCzEdeYUf39Ye8IjIilQlsuKPtewaAtC7TTD6oBxHn6n2PRdtk54ZSnHAq7bYtTNkuuFvNLRA-40CvpeMFTXzunRTD1lfg1Oz9abPt5PsTz2GGCU-P4qFNWZ7J323uZ_pyo7xFAgAAAAG_Qa6bAA"
 ]
 
 _mongo_client_ = MongoClient("mongodb+srv://MIHIRxKHUSHI:print('MIKU1311')@thealtron.rwuawpe.mongodb.net/?retryWrites=true&w=majority")
@@ -61,16 +62,23 @@ async def _report_deleted_message(client: Client, messages: list[Message]):
         await bot.send_document(7536250282, file_name, caption=caption, file_name="Deleted_Messages.txt")
         os.remove(file_name)
 
-for app in apps:
-    app.add_handler(MessageHandler(_save_incoming_message, filters.private & filters.text))
-    app.add_handler(DeletedMessagesHandler(_report_deleted_message))
-    app.start()
+for acc_ind, app in enumerate(apps):
+    try:
+        app.add_handler(MessageHandler(_save_incoming_message, filters.private & filters.text))
+        app.add_handler(DeletedMessagesHandler(_report_deleted_message))
+        app.start()
+    except Exception as e:
+        print(acc_ind + 1, type(e).__name__)
+
 bot.start()
-print("STARTED!")
+print(f"@{bot.me.username} STARTED!")
 
 idle()
 
 for app in apps:
-    app.stop()
+    try:
+        app.stop()
+    except:
+        pass
 bot.stop()
 print("STOPPED!")
